5f8bcfade69ceadd30b8989c7c84107a680dca19
Victor Moreira <victorsmoreira@outlook.com>
2022-05-07 16:42:10 -0300

    sign in with email and password

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,9 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth
+} from "../../utils/firebase/firebase.utils"
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -6,12 +11,12 @@ const SignIn = () => {
     const userDocRef = await createUserDocumentFromAuth(user);
   }
 
-
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Loginho</button>
-    </div>
+      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <SignUpForm />
+      </div>
   )
 }
 
